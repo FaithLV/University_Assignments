d51b48a8952410fe04073a8622f5ee8dae1e4e10
Faith <infinity1188@yahoo.com>
2018-02-18 01:25:50 +0200

    1.2.3

---
+++ 1_2_3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_2_3
+{
+    class Program
+    {
+        static Random rand = new Random();
+
+        static void Main(string[] args)
+        {
+            Pirmais();
+
+            Console.ReadLine();
+        }
+
+        static void Pirmais()
+        {
+            string[] array = new string[10];
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = randomString();
+            }
+
+            foreach (string str in array)
+            {
+                Console.WriteLine(str);
+            }
+        }
+
+        static string randomString(int lenght = 6)
+        {
+            int minChar = 97;
+            int maxChar = 122;
+            string output = String.Empty;
+
+            for(int i = 0; i < lenght; i++)
+            {
+                int randint = rand.Next(minChar, maxChar);
+                output = output + (char)randint;
+            }
+
+            return output;
+        }
+    }
+}
