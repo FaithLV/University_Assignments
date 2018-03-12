0b81b8401432b68b70f2d67c6a75d3a72db7f43a
Faith <infinity1188@yahoo.com>
2018-03-12 03:54:56 +0200

    5. uzd

---
+++ 1_2_5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Ievadi skaitli:");
+            //int input = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine($"Summa: {Sum(input)}");
+            Aritm(0, 3, 3);
+            Console.ReadLine();
+        }
+
+        static int Sum(int n)
+        {
+            if(n != 0)
+            {
+                return (n % 10 + Sum(n / 10));
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        
+
+    }
+}
